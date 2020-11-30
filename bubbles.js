1c37b8523df629ffe4691804fd08628b7a3496ee
chazpf <74632592+chazpf@users.noreply.github.com>
2020-11-30 05:20:50 -0600

    Tweaks

--- bubbles.js
+++ bubbles.js
@@ -1,43 +1,39 @@
 const scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
 
 
-function listScores (arr) {
+function list(arr) {
 	for (i = 0; i < arr.length; i++) {
 		console.log(`Bubble solution #${i} scores: ${arr[i]}`);
 	}	
-}
-
-function countTests (arr) {
 	console.log(`Bubbles tests: ${arr.length}`);
 }
 
-function printHighScore (arr) {
-	console.log(`Highest bubble score: ${Math.max(...arr)}`);
-}
-
-function printHSS (arr) {
-	const highScoreSolutions = [];
+function getHighScore(arr) {
+	let highScore = 0;
 	for (i = 0; i < arr.length; i++) {
-		if (arr[i] == Math.max(...arr) && highScoreSolutions.length == 0) {
-			highScoreSolutions.push(`#${i}`);
-		} else if (arr[i] == Math.max(...arr)) {
-			highScoreSolutions.push(` #${i}`);
+		if (arr[i] > highScore) {
+			highScore = arr[i];
 		}
 	}
-	console.log(`Solutions with highest score: ${highScoreSolutions}`);
+	return highScore;
 }
 
-function fullReport (arr) {
-	listScores(arr);
-	countTests(arr);
-	printHighScore(arr);
-	printHSS(arr);
+function printHSS(arr) {
+	const hSS = [];
+	for (i = 0; i < arr.length; i++) {
+		if (arr[i] == Math.max(...arr) && hSS.length == 0) {
+			hSS.push(`#${i}`);
+		} else if (arr[i] == Math.max(...arr)) {
+			hSS.push(` #${i}`);
+		}
+	}
+	return hSS;
 }
 
-function quickReport (arr) {
-	countTests(arr);
-	printHighScore(arr);
-	printHSS(arr);	
+function report(arr) {
+	console.log(`Highest bubble score: ${getHighScore(arr)}`);
+	console.log(`Solutions with highest score: ${printHSS(arr)}`);
 }
 
-fullReport(scores);
+list(scores);
+report(scores);
